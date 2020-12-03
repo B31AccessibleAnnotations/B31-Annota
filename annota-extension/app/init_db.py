171abdabf34737a2aa3b3c7502c97cbed5d24976
elysaia <jdiqiao@gmail.com>
2020-12-02 23:37:48 -0500

    mapview change source document title and timeline dummy text

--- annota-extension/app/init_db.py
+++ annota-extension/app/init_db.py
@@ -8,11 +8,11 @@ with open('schema.sql') as f:
 cur = connection.cursor()
 
 cur.execute("INSERT INTO files (title, content) VALUES (?, ?)",
-            ('Test', 'lorem ipsum')
+            ('Thesis Bill C-81 Notes', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
             )
 
 cur.execute("INSERT INTO files (title, content) VALUES (?, ?)",
-            ('Test 2', 'lorem ipsum')
+            ('Policy Analysis Study', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ')
             )
 
 connection.commit()
